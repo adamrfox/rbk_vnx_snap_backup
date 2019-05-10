18e2175876cd3dfcde760846f3536c42c77936bb
adamfox <adamrfox@yahoo.com>
2019-05-10 13:24:10 -0400

    Commented Added DEBUG mode Small fixes

--- rbk_vnx_snap_backup.py
+++ rbk_vnx_snap_backup.py
@@ -14,6 +14,7 @@ def usage():
     sys.stderr.write("Usage: rbk_vnx_snap_backup.py [-hvD] [-c creds] [-d data_mover] vnx filesystem path rubrik\n")
     sys.stderr.write("-h | --help : Prints this message\n")
     sys.stderr.write("-v | --verbose : Verbose mode, prints more messages\n")
+    sys.stderr.write("-D | --DEBUG : Debug mode.  Prints out more info\n")
     sys.stderr.write("-c | --creds= : Use a file for credentials\n")
     sys.stderr.write("-d | --data_mover= : Set the (virtual) data mover [default: server_2]\n")
     sys.stderr.write("vnx : Hostname or IP of the Control Station of the VNX\n")
@@ -45,7 +46,7 @@ def get_creds_from_file(file, array):
 
 def find_latest_ckpt(path):
     snaps = os.listdir(path + "/.ckpt")
-    latest = ["1970", "01" "01" "01.00.00"]
+    latest = "1970-01-01-01.00.00"
     for ckpt in snaps:
         if ckpt > latest:
             latest = ckpt
@@ -64,8 +65,9 @@ if __name__ == "__main__":
     rbk_ip_list = []
     dm = "server_2"
     verbose = False
+    DEBUG = False
 
-    optlist, args = getopt.getopt(sys.argv[1:], 'hvc:d:', ['--help', '--verbose', '--creds=', '--data_mover='])
+    optlist, args = getopt.getopt(sys.argv[1:], 'hvc:dD:', ['--help', '--verbose', '--creds=', '--data_mover=', ''--debug''])
     for opt, a in optlist:
         if opt in ('-h', '--help'):
             usage()
@@ -76,6 +78,8 @@ if __name__ == "__main__":
             (vnx_user, vnx_password) = get_creds_from_file(a, 'vnx')
         if opt in ('d', '--data_mover'):
             dm = a
+        if opt in ('-D', '--debug'):
+            DEBUG = True
     if args[0] == "?":
         usage()
     if user == "":
@@ -93,6 +97,9 @@ if __name__ == "__main__":
         share_type = "SMB"
         share_name = filesystem
         filesystem = "/" + filesystem
+
+# Set up Rubrik API Session and grab IP addresses if share is NFS
+
     rubrik = rubrik_cdm.Connect(rubrik_host, user, password)
     if share_type == "NFS":
         rbk_net = rubrik.get('internal', '/cluster/me/network_interface')
@@ -100,6 +107,9 @@ if __name__ == "__main__":
             for j in i['ipAddresses']:
                 rbk_ip_list.append(j)
         ip_list_str = ':'.join(rbk_ip_list)
+
+# Set up ssh session for VNX.  grab the current shares and find one with .ckpt in the path and delete it
+
     vnx = paramiko.SSHClient()
     vnx.set_missing_host_key_policy(paramiko.client.AutoAddPolicy)
     vnx.connect(vnx_host, username='root', password='nasadmin')
@@ -119,16 +129,23 @@ if __name__ == "__main__":
             else:
                 cmd = "export NAS_DB=/nas ; /nas/bin/server_export " + dm + " -Protocol cifs -unexport " + lf1[1]
             stdin, stdout, stderr = vnx.exec_command(cmd)
+
+# Find the latest checkpoint and create an export/share to it
+
     latest_ckpt = find_latest_ckpt(path)
     vprint("Creating export for " + latest_ckpt)
     if share_type == "NFS":
         cmd = "export NAS_DB=/nas ; /nas/bin/server_export " + dm + " -Protocol nfs -option access=" + ip_list_str + ":root=" + ip_list_str + " " + filesystem + "/.ckpt/" + latest_ckpt
     else:
         cmd = "export NAS_DB=/nas ; /nas/bin/server_export " + dm + " -Protocol cifs -name " + share_name + " " + filesystem + "/.ckpt/" + latest_ckpt
-    print cmd
+    if DEBUG:
+        print cmd
     stdin, stdout, stderr = vnx.exec_command(cmd)
-    rubrik_nfs = rubrik.get('internal','/host/share?share_type=' + share_type)
+
+# If NFS, update the export on Rubrik to point to the new ckpt path
+
     if share_type == "NFS":
+        rubrik_nfs = rubrik.get('internal','/host/share?share_type=' + share_type)
         for ex in rubrik_nfs['data']:
             export_path = str(ex['exportPoint'])
             if export_path.startswith(filesystem + "/.ckpt"):
